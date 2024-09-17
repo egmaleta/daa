deafdcc918fe6abe77099f64b75240d451129578
Eduardo García Maleta <egmaleta.secondary@proton.me>
2024-09-17 12:30:01 -0400

    finish impl (untested)

--- losing_friends/sln_1_bt.py
+++ losing_friends/sln_1_bt.py
@@ -6,9 +6,11 @@ class Graph:
             for _ in range(size)
         ]
     
+    def _is_valid_vertex(self, v: int):
+        return 0 <= v < len(self._mask)
+    
     def _is_valid_edge(self, v: int, w: int):
-        size = len(self._mask)
-        return v != w and 0 <= v < size and 0 <= w < size
+        return v != w and self._is_valid_vertex(v) and self._is_valid_vertex(w)
     
     def add_edge(self, v: int, w: int):
         assert self._is_valid_edge(v, w)
@@ -20,6 +22,10 @@ class Graph:
         self._mask[v][w] = False
         self._mask[w][v] = False
     
+    def edges(self, v: int):
+        assert self._is_valid_vertex(v)
+        return [w for w, has_edge in enumerate(self._mask[v]) if has_edge]
+    
     def edge_count(self):
         count = 0
         for v, mask in enumerate(self._mask):
@@ -27,6 +33,39 @@ class Graph:
                 if has_edge:
                     count += 1
         return count
+    
+    def vertices(self):
+        return [v for v in range(len(self._mask))]
+    
+    def traverse(self, v_start: int, w_start: int):
+        for v, mask in enumerate(self._mask[v_start:], start=v_start):
+            if v > v_start:
+                # continue traversing matrix normally
+                w_start = v + 1
+            
+            for w, has_edge in enumerate(mask[w_start:], start=w_start):
+                yield v, w, has_edge
+
+
+class _SlnFound(Exception):
+    pass
+
+
+def bt(g: Graph, v_start: int, w_start: int, friendship_count: int):
+    if friendship_count == 0:
+        return
+    
+    edges_list = [g.edges(v) for v in g.vertices()]
+    if all(len(edges) == 3 for edges in edges_list):
+        raise _SlnFound()
+    if all(len(edges) == 0 for edges in edges_list):
+        raise _SlnFound()
+    
+    for v, w, has_edge in g.traverse(v_start, w_start):
+        if has_edge:
+            g.remove_edge(v, w)
+            bt(g, v, w + 1, friendship_count - 1)
+            g.add_edge(v, w)
 
 
 def sln(k: int, friendships: list[tuple[int, int]]):
@@ -34,12 +73,9 @@ def sln(k: int, friendships: list[tuple[int, int]]):
     for v, w in friendships:
         g.add_edge(v, w)
     
-    friendship_count = g.edge_count()
-    possible = False
+    try:
+        bt(g, 0, 1, g.edge_count())
+    except _SlnFound:
+        return True
 
-    def bt(g: Graph, v: int, w: int):
-        raise NotImplementedError()
-
-    bt(g, 0, 0)
-
-    return possible
+    return False
