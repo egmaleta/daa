d95053eb131208df13a9756fe9498f909a67e1c4
Eduardo García Maleta <egmaleta.secondary@proton.me>
2024-09-17 22:11:26 -0400

    impl floyd warshall algo

--- shared/graph.py
+++ shared/graph.py
@@ -47,3 +47,41 @@ class Graph:
     
     def vertices(self):
         return [v for v in range(len(self._mask))]
+
+
+class DirectedGraph:
+    def __init__(self, size: int):
+        assert size >= 2
+        self._mask: list[list[Any]] = [
+            [None for _ in range(size)]
+            for _ in range(size)
+        ]
+    
+    def traverse(self, v_start = 0, w_start = 0):
+        for v, mask in enumerate(self._mask[v_start:], start=v_start):
+            if v > v_start:
+                # continue traversing matrix normally
+                w_start = 0
+            
+            for w, data in enumerate(mask[w_start:], start=w_start):
+                if w == v:
+                    continue
+                yield v, w, data
+    
+    def _is_valid_vertex(self, v: int):
+        return 0 <= v < len(self._mask)
+    
+    def _is_valid_edge(self, v: int, w: int):
+        return v != w and self._is_valid_vertex(v) and self._is_valid_vertex(w)
+    
+    def add_edge(self, v: int, w: int, data):
+        assert self._is_valid_edge(v, w)
+        assert data is not None
+        self._mask[v][w] = data
+
+    def remove_edge(self, v: int, w: int):
+        assert self._is_valid_edge(v, w)
+        self._mask[v][w] = None
+    
+    def V(self):
+        return [v for v in range(len(self._mask))]
